7df8e216c36613c700ee6d83741848ba8efd5684
Planteig <Planteig17@hotmail.com>
2024-12-03 13:54:33 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+import requests
+import sqlite3
+import bcrypt
+import os
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from dotenv import load_dotenv
+from flasgger import swag_from
+
+app = Flask(__name__)
+
+
+
+
+
+app.run(debug=True, host="0.0.0.0")
